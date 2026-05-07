4b072a8e033f0b42be11fb01713b69afc4f918fc
null <null>
2026-05-07 09:02:53 +0200

    Restaurar redireccion principal

--- next.config.js
+++ next.config.js
@@ -1,14 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // async redirects() {
-  //   return [
-  //     {
-  //       source: '/',
-  //       destination: '/reserva',
-  //       permanent: true,
-  //     },
-  //   ]
-  // },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/reserva',
+        permanent: true,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
